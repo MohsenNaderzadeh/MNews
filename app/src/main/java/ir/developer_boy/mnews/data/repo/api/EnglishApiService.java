dbb32ded4bbed629f490184f0e08b57f7c4074af
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-20 19:32:08 +0430

    first commit- add Interfaces for all requests related to english content

---
+++ app/src/main/java/ir/developer_boy/mnews/data/repo/api/EnglishApiService.java
@@ -0,0 +1,19 @@
+package ir.developer_boy.mnews.data.repo.api;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import ir.developer_boy.mnews.data.Banners;
+import ir.developer_boy.mnews.data.News;
+import retrofit2.http.GET;
+
+public interface EnglishApiService {
+
+    @GET("GetNewsToEnglish.php")
+    Single<List<News>> getNews();
+    @GET("GetNewsToPersian.php")
+    Single<List<Banners>> getBanners();
+
+    @GET("GetNewsToEnglish.php")
+    Single<List<News>> getVideoNews();
+}
