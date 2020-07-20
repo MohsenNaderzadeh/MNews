3598aeb131ab8322aec49a071f8700f9d129e9cf
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-20 19:45:16 +0430

    first commit- change extendation to our base activity

--- app/src/main/java/ir/developer_boy/mnews/MainActivity.java
+++ app/src/main/java/ir/developer_boy/mnews/MainActivity.java
@@ -1,12 +1,13 @@
-package ir.developer_boy.mnews.main;
+package ir.developer_boy.mnews;
 
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import ir.developer_boy.mnews.R;
+import ir.developer_boy.mnews.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
